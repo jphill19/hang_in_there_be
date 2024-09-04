188df375726480d266c8c31b33f63bc6e610d335
John Hill <jphill1989@gmail.com>
2024-09-04 16:15:40 -0600

    Feat: Added create_a_poster & tests

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -5,6 +5,23 @@ class Api::V1::PostersController < ApplicationController
   end
   def show
     poster = Poster.find(params[:id])
-    render json: PosterSerializer.format_posters([ poster ])
+    render json: PosterSerializer.format_posters([poster])
   end
+
+  def create
+    poster = Poster.create(poster_params)
+    render json: PosterSerializer.format_posters([poster])
+  end
+
+  def update 
+    poster = Poster.find(params[:id])
+    poster.update(poster_params)
+    render json: PosterSerializer.format_posters([poster])
+  end
+
+  private
+
+  def poster_params
+    params.require(:poster).permit(:name, :description, :price, :year, :vintage, :img_url)
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
 
   get "/api/v1/posters", to: "api/v1/posters#index"
   get "/api/v1/posters/:id", to: "api/v1/posters#show"
+  patch "/api/v1/posters/:id", to: "api/v1/posters#update"
+  post "/api/v1/posters", to: "api/v1/posters#create"
 end
