4dc78ecf2e801e6c497e67d2510c0303ad4dbe5b
John Hill <jphill1989@gmail.com>
2024-09-05 13:55:21 -0600

    Removed comments from code

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -1,15 +1,5 @@
 class Api::V1::PostersController < ApplicationController
   def index
-    # refactor
-    # binding.pry
-    # if params[:sort] == 'asc'
-    #   posters = Poster.order(created_at: :asc)
-    # elsif params[:sort] == 'desc'
-    #   posters = Poster.order(created_at: :desc)
-    # else
-    #   posters = Poster.all
-    # end
-
     posters = Poster.all
                           .sort_asc(params[:sort])
                           .sort_dsc(params[:sort])
