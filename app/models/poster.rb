1ef993c4765ece9aae985082337f313c41d17c7a
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-09-05 16:14:53 -0400

    Added filter_by_name in the controller and Poster class

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -1,4 +1,5 @@
 class Poster < ApplicationRecord
   scope :sort_asc, -> (sort) {order(created_at: :asc) if sort=="asc"}
   scope :sort_dsc, -> (sort) {order(created_at: :desc) if sort=="desc"}
+  scope :filter_by_name, -> (name) {where("name ILIKE '%#{name}%'") if name.present?}
 end
